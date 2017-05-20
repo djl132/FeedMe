c603d6dfe73000c8e3e777a3b5bc119a574e609d
DerekLin <djl132@case.edu>
2017-05-19 17:50:40 -0700

    FINISHED BASIC RESTFUL UI AND ROUTES AND MODELS, GOING TO INTEGRATE STREAM.IO

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :views
+  get 'page/index'
+
+  resources :items, only: [:new, :create]
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#index'
+
 end
